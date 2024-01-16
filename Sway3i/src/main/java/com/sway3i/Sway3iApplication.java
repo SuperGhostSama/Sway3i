d7e9baaf0a477e86e246b0281ad1315d26df3400
Salah <salaheddinehanaoui@gmail.com>
2024-01-16 16:37:26 +0100

    made a modelMapper bean

--- Sway3i/src/main/java/com/sway3i/Sway3iApplication.java
+++ Sway3i/src/main/java/com/sway3i/Sway3iApplication.java
@@ -1,13 +1,22 @@
 package com.sway3i;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Sway3iApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Sway3iApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }

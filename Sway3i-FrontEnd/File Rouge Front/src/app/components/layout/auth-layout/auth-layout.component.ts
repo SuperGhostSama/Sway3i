4d341c8e9a56e002cc39608fbbd1a496888f3353
Salah <salaheddinehanaoui@gmail.com>
2024-03-06 09:50:06 +0100

    updated the navbar

--- Sway3i-FrontEnd/File Rouge Front/src/app/components/layout/auth-layout/auth-layout.component.ts
+++ Sway3i-FrontEnd/File Rouge Front/src/app/components/layout/auth-layout/auth-layout.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.css']
 })
-export class AuthLayoutComponent {
+export class AuthLayoutComponent{
+
+ 
 
 }

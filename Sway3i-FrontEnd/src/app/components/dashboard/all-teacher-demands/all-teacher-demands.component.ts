ec1db9d0dc3ae5a9220e09b984608e3c5b169fc1
Salah <salaheddinehanaoui@gmail.com>
2024-03-18 17:47:27 +0000

    started showing all the teacher demands

--- Sway3i-FrontEnd/src/app/components/dashboard/all-teacher-demands/all-teacher-demands.component.ts
+++ Sway3i-FrontEnd/src/app/components/dashboard/all-teacher-demands/all-teacher-demands.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TeacherDemandResponseDTO } from 'src/app/dto/TeacherDemand/responses/teacher-demand-response-DTO';
+import { TeacherDemandService } from 'src/app/services/TeacherDemand/teacher-demand.service';
 
 @Component({
   selector: 'app-all-teacher-demands',
@@ -6,8 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./all-teacher-demands.component.css']
 })
 export class AllTeacherDemandsComponent {
-  createdById: number = 10;
-  subject: string = "Mathematics";
-  educationLevel: string = "HIGH_SCHOOL";
-  description: string = "Looking for a teacher for advanced math classes";
+  teacherDemands: TeacherDemandResponseDTO[] = [];
+
+  constructor(private teacherDemandService: TeacherDemandService) {}
+
+  ngOnInit(): void {
+    this.getAllTeacherDemands();
+  }
+
+  getAllTeacherDemands(): void {
+    this.teacherDemandService.getAllTeacherDemands().subscribe(
+      (teacherDemands: TeacherDemandResponseDTO[]) => {
+        this.teacherDemands = teacherDemands;
+        console.log('Teacher demands:', teacherDemands);
+      },
+      (error) => {
+
+        console.error('Error fetching teacher demands:', error);
+      }
+    );
+  }
 }

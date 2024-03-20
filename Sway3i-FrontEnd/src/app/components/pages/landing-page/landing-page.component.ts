292ad996193e874b08a6f7cca8f1ba4a694d1b9a
Salah <salaheddinehanaoui@gmail.com>
2024-03-20 14:05:16 +0000

    made the filter for landing page

--- Sway3i-FrontEnd/src/app/components/pages/landing-page/landing-page.component.ts
+++ Sway3i-FrontEnd/src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 import { CourseWithDetailsResponseDTO } from 'src/app/dto/Course/responses/course-with-details-response-DTO';
+import { Course } from 'src/app/modal/entities/course';
 import { CourseService } from 'src/app/services/Course/course.service';
 
 @Component({
@@ -10,15 +11,20 @@ import { CourseService } from 'src/app/services/Course/course.service';
 export class LandingPageComponent {
 
   latestCourses: CourseWithDetailsResponseDTO[] = [];
+  filteredCourses: CourseWithDetailsResponseDTO[] = [];
+
 
   constructor(
     private courseService: CourseService
     ) { }
 
-    
+
   ngOnInit(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+
     this.getAllLatestCourses();
+    
   }
 
   getAllLatestCourses(): void {
@@ -34,4 +40,7 @@ export class LandingPageComponent {
   }
 
 
+  filterCourses(educationLevel: string): void {
+    this.filteredCourses = this.latestCourses.filter(course => course.educationLevel === educationLevel);
+  }
 }

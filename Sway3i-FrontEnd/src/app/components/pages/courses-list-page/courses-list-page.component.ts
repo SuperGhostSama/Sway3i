7743cb4c40af98ed87a13eecb44577ff500f06d5
Salah <salaheddinehanaoui@gmail.com>
2024-03-20 09:55:59 +0000

    started showing all courses in the courses list page

--- Sway3i-FrontEnd/src/app/components/pages/courses-list-page/courses-list-page.component.ts
+++ Sway3i-FrontEnd/src/app/components/pages/courses-list-page/courses-list-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseWithDetailsResponseDTO } from 'src/app/dto/Course/responses/course-with-details-response-DTO';
+import { CourseService } from 'src/app/services/Course/course.service';
+import { NotificationService } from 'src/app/services/Notification/notification.service';
 
 @Component({
   selector: 'app-courses-list-page',
   templateUrl: './courses-list-page.component.html',
   styleUrls: ['./courses-list-page.component.css']
 })
-export class CoursesListPageComponent {
+export class CoursesListPageComponent{
+
+
+
 
 }
--- Sway3i-FrontEnd/src/app/components/utils/courses-cards/courses-cards.component.ts
+++ Sway3i-FrontEnd/src/app/components/utils/courses-cards/courses-cards.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CourseWithDetailsResponseDTO } from 'src/app/dto/Course/responses/course-with-details-response-DTO';
+import { CourseService } from 'src/app/services/Course/course.service';
+import { NotificationService } from 'src/app/services/Notification/notification.service';
 
 @Component({
   selector: 'app-courses-cards',
   templateUrl: './courses-cards.component.html',
   styleUrls: ['./courses-cards.component.css']
 })
-export class CoursesCardsComponent {
+export class CoursesCardsComponent implements OnInit{
 
+  courses: CourseWithDetailsResponseDTO[] = [];
+
+  constructor(
+    private courseService: CourseService,
+    private notificationService: NotificationService
+    ) { }
+
+  ngOnInit(): void {
+    this.getAllCourses();
+  }
+
+  getAllCourses(): void {
+    this.courseService.getAllCourses().subscribe(
+      (courses: CourseWithDetailsResponseDTO[]) => {
+        this.courses = courses;
+        console.log('Courses:', this.courses);
+      },
+      (error) => {
+        console.error('Error fetching courses:', error);
+      }
+    );
+  }
 }

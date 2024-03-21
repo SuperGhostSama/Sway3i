5b45e58f86499e2da557a8f69e8da3af10c18da4
Salah <salaheddinehanaoui@gmail.com>
2024-03-21 10:30:02 +0000

    started showing inventory

--- Sway3i-FrontEnd/src/app/components/dashboard/inventory/inventory.component.ts
+++ Sway3i-FrontEnd/src/app/components/dashboard/inventory/inventory.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EnrolledCourseResponseDTO } from 'src/app/dto/StudentInCourse/responses/enrolled-course-response-DTO';
+import { StudentsInCourseService } from 'src/app/services/StudentInCourse/student-in-course.service';
 
 @Component({
   selector: 'app-inventory',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent {
+
+  enrolledCourses: EnrolledCourseResponseDTO[] = [];
+
+  constructor(
+    private router: Router,
+    private studentsInCourseService: StudentsInCourseService
+  ) { }
+
+  ngOnInit(): void {
+    const studentId = Number(localStorage.getItem('id'));
+
+    if (!isNaN(studentId)) {
+
+      this.studentsInCourseService.getEnrolledCoursesByStudentId(studentId)
+        .subscribe(
+          (courses: EnrolledCourseResponseDTO[]) => {
+            this.enrolledCourses = courses;
+            console.log(this.enrolledCourses);
+          },
+          (error) => {
+            console.error('Error fetching enrolled courses:', error);
+          }
+        );
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
 
 }
--- Sway3i-FrontEnd/src/app/dto/StudentInCourse/responses/enrolled-course-response-DTO.ts
+++ Sway3i-FrontEnd/src/app/dto/StudentInCourse/responses/enrolled-course-response-DTO.ts
@@ -6,11 +6,14 @@ export interface EnrolledCourseResponseDTO {
     isExpired: boolean;
     studentId: number;
     courseId: number;
+    courseName: string;
     courseDetails: string;
     courseSubject: string;
     createdByFirstName: string;
     createdByLastName: string;
     createdByCity: string;
     pricingPlan: PricingPlan; 
+    link: string;
+    address: string;
   }
   
